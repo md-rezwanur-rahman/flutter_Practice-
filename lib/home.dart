a441e0f4e633d8a31cc7484693d44e2c8a080f84
Rezwan <rayjan4567@gmail.com>
2025-09-29 01:37:31 +0600

    first writing

---
+++ lib/home.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('My app',
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.w500
+        ),
+        ),
+        centerTitle: true,
+      ),
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          
+          Text('This is His app',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w500,
+            color: Colors.purple,
+            wordSpacing: 5
+          ),
+          ),
+          Text('This is Her app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Rayjan app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Sadat app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is His app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Her app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Rayjan app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Sadat app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is His app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Her app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Rayjan app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Sadat app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is His app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Her app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Rayjan app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Sadat app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is His app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Her app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Rayjan app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          ),
+          Text('This is Sadat app',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w500,
+                color: Colors.purple,
+                wordSpacing: 5
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
